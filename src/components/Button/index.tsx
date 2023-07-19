3c93ee46bfbbb9135ec0226ab4d3bc2ab01784ec
markoclimako <mkclimako@gmail.com>
2023-07-18 21:31:52 -0300

    feat(components): implement Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import styles from './button.module.css';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string
+  id: string
+  variantGreen?: boolean
+}
+function Button({ title, id, variantGreen = false, ...rest }:ButtonProps) {
+  return (
+    <button
+      className={ `${variantGreen && styles.button_Green} ${styles.button}` }
+      data-testid={ id }
+      { ...rest }
+    >
+      {title}
+    </button>
+  );
+}
+
+export default Button;
