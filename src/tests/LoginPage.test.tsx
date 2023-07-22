3f725a70aee27e7c00a25be136e3a73cf1f3df8c
markoclimako <mkclimako@gmail.com>
2023-07-22 16:54:01 -0300

    test: implement test cases for Login Page
    Implement UI test for Login Page:
    - Should have a logo
    - Should have an email input field
    - Should have a login button
    
    Implement User Actions Test on Login Page:
    - Should fill in the email and password fields with typed text
    - Should disable the button if both email and password are invalid
    - Should disable the button if email is invalid and password is valid
    - Should disable the button if email is valid and password is invalid
    - Should enable the button if both email and password are valid
    - Should be directed to '/carteira'

--- src/tests/LoginPage.test.tsx
+++ src/tests/LoginPage.test.tsx
@@ -1,77 +1,149 @@
 import { screen } from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
-import App from '../App';
-import Login from '../pages/Login';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
-import { EMAIL_INITAL_STATE, EMAIL_INPUT, INVALID_TEST_EMAIL, INVALID_TEST_PASSWORD, LOGIN_BUTTON, PASSWORD_INITAL_STATE, PASSWORD_INPUT, VALID_TEST_EMAIL, VALID_TEST_PASSWORD } from './utils/constantes';
 
-describe('LoginForm component tests', () => {
-  it('should render the login form with initial state', () => {
+import App from '../App';
+import LoginForm from '../components/LoginForm';
+import Login from '../pages/Login';
+import { INVALID_EMAIL, INVALID_PASSWORD, LOGIN_BUTTON, LOGIN_EMAIL_INPUT_ID, LOGIN_PASSWORD_INPUT_ID, LOGO_ID, VALID_EMAIL, VALID_PASSWORD } from './utils/constantes';
+
+describe('Test UI Elements on Login Page', () => {
+  it('should have a logo', () => {
     renderWithRouterAndRedux(<Login />);
 
-    const email = screen.getByTestId(EMAIL_INPUT);
-    const password = screen.getByTestId(PASSWORD_INPUT);
-    const loginBtn = screen.getByRole('button', { name: LOGIN_BUTTON });
-
-    expect(email).toHaveValue(EMAIL_INITAL_STATE);
-    expect(password).toHaveValue(PASSWORD_INITAL_STATE);
-
-    expect(loginBtn).toBeDisabled();
+    const logo = screen.getByTestId(LOGO_ID);
+    expect(logo).toBeInTheDocument();
   });
 
-  it('should disable the login button when the email or password are invalid', async () => {
+  it('should have an email input field', () => {
     renderWithRouterAndRedux(<Login />);
 
-    const email = screen.getByTestId(EMAIL_INPUT);
-    const password = screen.getByTestId(PASSWORD_INPUT);
-    const loginBtn = screen.getByRole('button', { name: LOGIN_BUTTON });
-
-    expect(loginBtn).toBeDisabled();
-
-    await userEvent.type(email, INVALID_TEST_EMAIL);
-    await userEvent.type(password, VALID_TEST_PASSWORD);
-
-    expect(loginBtn).toBeDisabled();
-
-    userEvent.clear(email);
-    userEvent.clear(password);
-
-    await userEvent.type(email, VALID_TEST_EMAIL);
-    await userEvent.type(password, INVALID_TEST_PASSWORD);
-
-    expect(loginBtn).toBeDisabled();
+    const emailInput = screen.getByTestId(LOGIN_EMAIL_INPUT_ID);
+    expect(emailInput).toBeInTheDocument();
   });
 
-  it('should enable the login button when the email and password are valid', async () => {
+  it('should have a password input field', () => {
     renderWithRouterAndRedux(<Login />);
 
-    const email = screen.getByTestId(EMAIL_INPUT);
-    const password = screen.getByTestId(PASSWORD_INPUT);
-    const loginBtn = screen.getByRole('button', { name: LOGIN_BUTTON });
-
-    expect(loginBtn).toBeDisabled();
-
-    await userEvent.type(email, VALID_TEST_EMAIL);
-    await userEvent.type(password, VALID_TEST_PASSWORD);
-
-    expect(loginBtn).toBeEnabled();
+    const passwordInput = screen.getByTestId(LOGIN_PASSWORD_INPUT_ID);
+    expect(passwordInput).toBeInTheDocument();
   });
 
-  it('should redirect to "/carteira" when the login button is clicked', async () => {
+  it('should have a login button', () => {
+    renderWithRouterAndRedux(<Login />);
+
+    const loginButton = screen.getByRole('button', { name: LOGIN_BUTTON });
+    expect(loginButton).toBeInTheDocument();
+  });
+});
+
+describe('User Actions on Login Page', () => {
+  it('should fill in the email and password fields with typed text', async () => {
+    renderWithRouterAndRedux(<LoginForm />);
+    const emailInput = screen.getByTestId(LOGIN_EMAIL_INPUT_ID);
+    const passwordInput = screen.getByTestId(LOGIN_PASSWORD_INPUT_ID);
+
+    expect(emailInput).toHaveValue('');
+    expect(passwordInput).toHaveValue('');
+
+    await userEvent.type(emailInput, VALID_EMAIL);
+    await userEvent.type(passwordInput, VALID_PASSWORD);
+
+    expect(emailInput).toHaveValue(VALID_EMAIL);
+    expect(passwordInput).toHaveValue(VALID_PASSWORD);
+  });
+
+  it('should disable the button if both email and password are invalid', async () => {
+    renderWithRouterAndRedux(<LoginForm />);
+    const emailInput = screen.getByTestId(LOGIN_EMAIL_INPUT_ID);
+    const passwordInput = screen.getByTestId(LOGIN_PASSWORD_INPUT_ID);
+    const loginButton = screen.getByRole('button', { name: LOGIN_BUTTON });
+
+    expect(emailInput).toHaveValue('');
+    expect(passwordInput).toHaveValue('');
+
+    await userEvent.type(emailInput, INVALID_EMAIL);
+    await userEvent.type(passwordInput, INVALID_PASSWORD);
+
+    expect(emailInput).toHaveValue(INVALID_EMAIL);
+    expect(passwordInput).toHaveValue(INVALID_PASSWORD);
+
+    expect(loginButton).toBeDisabled();
+  });
+
+  it('should disable the button if email is invalid and password is valid', async () => {
+    renderWithRouterAndRedux(<LoginForm />);
+    const emailInput = screen.getByTestId(LOGIN_EMAIL_INPUT_ID);
+    const passwordInput = screen.getByTestId(LOGIN_PASSWORD_INPUT_ID);
+    const loginButton = screen.getByRole('button', { name: LOGIN_BUTTON });
+
+    expect(emailInput).toHaveValue('');
+    expect(passwordInput).toHaveValue('');
+
+    await userEvent.type(emailInput, INVALID_EMAIL);
+    await userEvent.type(passwordInput, VALID_PASSWORD);
+
+    expect(emailInput).toHaveValue(INVALID_EMAIL);
+    expect(passwordInput).toHaveValue(VALID_PASSWORD);
+
+    expect(loginButton).toBeDisabled();
+  });
+
+  it('should disable the button if email is valid and password is invalid', async () => {
+    renderWithRouterAndRedux(<LoginForm />);
+    const emailInput = screen.getByTestId(LOGIN_EMAIL_INPUT_ID);
+    const passwordInput = screen.getByTestId(LOGIN_PASSWORD_INPUT_ID);
+    const loginButton = screen.getByRole('button', { name: LOGIN_BUTTON });
+
+    expect(emailInput).toHaveValue('');
+    expect(passwordInput).toHaveValue('');
+
+    await userEvent.type(emailInput, VALID_EMAIL);
+    await userEvent.type(passwordInput, INVALID_PASSWORD);
+
+    expect(emailInput).toHaveValue(VALID_EMAIL);
+    expect(passwordInput).toHaveValue(INVALID_PASSWORD);
+
+    expect(loginButton).toBeDisabled();
+  });
+
+  it('should enable the button if both email and password are valid', async () => {
+    renderWithRouterAndRedux(<LoginForm />);
+    const emailInput = screen.getByTestId(LOGIN_EMAIL_INPUT_ID);
+    const passwordInput = screen.getByTestId(LOGIN_PASSWORD_INPUT_ID);
+    const loginButton = screen.getByRole('button', { name: LOGIN_BUTTON });
+
+    expect(emailInput).toHaveValue('');
+    expect(passwordInput).toHaveValue('');
+
+    await userEvent.type(emailInput, VALID_EMAIL);
+    await userEvent.type(passwordInput, VALID_PASSWORD);
+
+    expect(emailInput).toHaveValue(VALID_EMAIL);
+    expect(passwordInput).toHaveValue(VALID_PASSWORD);
+
+    expect(loginButton).toBeEnabled();
+  });
+
+  it('should be directed to "/carteira" when clicking the "entrar" button if it is enabled', async () => {
     renderWithRouterAndRedux(<App />);
+    const emailInput = screen.getByTestId(LOGIN_EMAIL_INPUT_ID);
+    const passwordInput = screen.getByTestId(LOGIN_PASSWORD_INPUT_ID);
+    const loginButton = screen.getByRole('button', { name: LOGIN_BUTTON });
 
-    const email = screen.getByTestId(EMAIL_INPUT);
-    const password = screen.getByTestId(PASSWORD_INPUT);
-    const loginBtn = screen.getByRole('button', { name: LOGIN_BUTTON });
+    expect(emailInput).toHaveValue('');
+    expect(passwordInput).toHaveValue('');
 
-    await userEvent.type(email, VALID_TEST_EMAIL);
-    await userEvent.type(password, VALID_TEST_PASSWORD);
+    await userEvent.type(emailInput, VALID_EMAIL);
+    await userEvent.type(passwordInput, VALID_PASSWORD);
 
-    expect(loginBtn).toBeEnabled();
+    expect(emailInput).toHaveValue(VALID_EMAIL);
+    expect(passwordInput).toHaveValue(VALID_PASSWORD);
 
-    await userEvent.click(loginBtn);
+    expect(loginButton).toBeEnabled();
 
-    const userEmail = screen.getByRole('heading', { name: /alguem@email\.com/i });
+    await userEvent.click(loginButton);
+    const userEmail = screen.getByRole('heading', { name: VALID_EMAIL });
     expect(userEmail).toBeInTheDocument();
   });
 });
