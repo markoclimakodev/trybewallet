c3789b768eb1829215479cf0786997b050b2a5ed
markoclimako <mkclimako@gmail.com>
2023-07-23 08:44:35 -0300

    test: getting beaten by the tests... again 4

--- src/tests/WalletPage.spec.tsx
+++ src/tests/WalletPage.spec.tsx
@@ -1,47 +1,41 @@
 import { screen, waitFor } from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
-import App from '../App';
 import Wallet from '../pages/Wallet';
+
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 import { dataOnScreen1, formFilledData1, initalValues } from './mocks/filledData';
-import { BTN_CANCEL_EDIT, BTN_DELETE, BTN_EDIT_EXPENSE, LOGIN_BUTTON, VALID_TEST_EMAIL, VALID_TEST_PASSWORD } from './utils/constantes';
-import { assertExpensesWereAdded, assertExpensesWereDeleted, assertFormValues, checkLoginButtonIsEnabled, checkUIElementsArePresent, simulateUserActivingEditMode, simulateUserClickingOnAddExpenseButton, simulateUserTypingOnExpenseForm, simulateUserTypingOnLoginPage } from './utils/helperFunctions';
+import { BTN_CANCEL_EDIT, BTN_DELETE, BTN_EDIT_EXPENSE } from './utils/constantes';
+import {
+  assertExpensesWereAdded,
+  assertExpensesWereDeleted,
+  assertFormValues,
+  checkUIElementsArePresent,
+  simulateUserActivingEditMode,
+  simulateUserClickingOnAddExpenseButton,
+  simulateUserTypingOnExpenseForm,
+} from './utils/helperFunctions';
+
+beforeEach(async () => {
+  renderWithRouterAndRedux(<Wallet />);
+  await assertFormValues(initalValues);
+});
 
 describe('Test Wallet Page in Register Mode', () => {
   it('should renders the wallet page with all ui elements', async () => {
-    renderWithRouterAndRedux(<App />);
-
-    const loginBtn = screen.getByRole('button', { name: LOGIN_BUTTON });
-    await simulateUserTypingOnLoginPage(VALID_TEST_EMAIL, VALID_TEST_PASSWORD);
-    checkLoginButtonIsEnabled();
-
-    await userEvent.click(loginBtn);
-
     checkUIElementsArePresent();
   });
 
-  it('should have the fields filled with initial values', async () => {
-    renderWithRouterAndRedux(<Wallet />);
-    await assertFormValues(initalValues);
-  });
-
   it('should fill in the fields with typed text and selected options', async () => {
-    renderWithRouterAndRedux(<Wallet />);
-    await assertFormValues(initalValues);
     await simulateUserTypingOnExpenseForm(formFilledData1);
     await assertFormValues(formFilledData1);
   });
 
   it('should fill in the fields with typed text and selected options', async () => {
-    renderWithRouterAndRedux(<Wallet />);
-    await assertFormValues(initalValues);
     await simulateUserTypingOnExpenseForm(formFilledData1);
     await assertFormValues(formFilledData1);
   });
 
   it('the user can add a new expense when clicked the button "adicionar despesa', async () => {
-    renderWithRouterAndRedux(<Wallet />);
-    await assertFormValues(initalValues);
     await simulateUserTypingOnExpenseForm(formFilledData1);
     await assertFormValues(formFilledData1);
     await simulateUserClickingOnAddExpenseButton();
@@ -49,8 +43,6 @@ describe('Test Wallet Page in Register Mode', () => {
   });
 
   it('the user can delete an expense when clicked the delete button', async () => {
-    renderWithRouterAndRedux(<Wallet />);
-    await assertFormValues(initalValues);
     await simulateUserTypingOnExpenseForm(formFilledData1);
     await assertFormValues(formFilledData1);
     await simulateUserClickingOnAddExpenseButton();
@@ -66,8 +58,6 @@ describe('Test Wallet Page in Register Mode', () => {
 
 describe('Test Wallet Page in Register Mode', () => {
   it('should change the ExpenseForm to edit mode', async () => {
-    renderWithRouterAndRedux(<Wallet />);
-    await assertFormValues(initalValues);
     await simulateUserTypingOnExpenseForm(formFilledData1);
     await assertFormValues(formFilledData1);
     await simulateUserClickingOnAddExpenseButton();
